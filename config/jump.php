ccada038338a40f3825fb32aadf801f6aca6941b
JefferyCai <1345199080@qq.com>
2019-12-27 10:21:40 +0800

    更新最新文件

--- config/jump.php
+++ config/jump.php
@@ -7,10 +7,10 @@
  */
 return[
     // 默认跳转页面对应的模板文件
-//    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
-//    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
 
 // kplphp自定义跳转
-    'dispatch_success_tmpl' => app()->getRootPath().'/web/view/admin/dispatch_jump.tpl',
-    'dispatch_error_tmpl'   => app()->getRootPath().'/web/view/admin/dispatch_jump.tpl',
+//    'dispatch_success_tmpl' => app()->getRootPath().'/web/view/admin/dispatch_jump.tpl',
+//    'dispatch_error_tmpl'   => app()->getRootPath().'/web/view/admin/dispatch_jump.tpl',
 ];
