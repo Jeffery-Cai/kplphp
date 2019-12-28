2e2efd8bd22b43dac7599be9767cd597d9ea618d
JefferyCai <1345199080@qq.com>
2019-12-28 18:17:22 +0800

    新增kplphp核心文件

---
+++ config/kplphp.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'product_name'      => 'kplphp',
+    'product_version'   => '1.0.0',
+    'build_version'     => '201912281516',
+    'product_website'   => 'http://www.kplphp.com',
+    'product_update'    => 'http://www.kplphp.com/web/update',
+];
